fa580974c1edade89e6a396a6aee49e0c742e977
EugenSusurrus <39225487+EugenSusurrus@users.noreply.github.com>
2018-11-07 12:17:17 +0100

    Add files via upload

---
+++ inc.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 11:57:33 2018
+
+@author: eugenr
+"""
+
+def inc(inputNum):
+    return inputNum + 1
+
+print(inc(10))
---
+++ incdemo_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  7 12:06:16 2018
+
+@author: eugenr
+"""
+
+import unittest
+import inc
+
+class MyTest(unittest.TestCase):
+    def test_my_function(self):
+        self.assertEqual(inc(0),1)
+        self.assertEqual(inc(100),101)
+        self.assertEqual(inc(1), 2)
+        self.assertEqual(inc(-1),0)
+        self.assertEqual(inc(4), 4)
+        
+if __name__ == '__main__':
+    unittest.main()
+    
