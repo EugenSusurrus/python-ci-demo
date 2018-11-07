dfee0e717dcfcd728c0e092a374ab007278aaf64
EugenSusurrus <39225487+EugenSusurrus@users.noreply.github.com>
2018-11-07 12:55:54 +0100

    Update incdemo_test.py

--- incdemo_test.py
+++ incdemo_test.py
@@ -6,14 +6,14 @@ Created on Wed Nov  7 12:06:16 2018
 """
 
 import unittest
-import inc
+from inc import inc
 
 class MyTest(unittest.TestCase):
     def test_my_function(self):
-        self.assertEqual(inc(0),1)
-        self.assertEqual(inc(100),101)
+        self.assertEqual(inc(0), 1)
+        self.assertEqual(inc(100), 101)
         self.assertEqual(inc(1), 2)
-        self.assertEqual(inc(-1),0)
+        self.assertEqual(inc(-1), 0)
         
 if __name__ == '__main__':
     unittest.main()
